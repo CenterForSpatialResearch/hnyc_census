19875958384796a4af58fd36b186e9d440a5edf0
Panchanok <panchanok@panchanoks-air.home>
2019-11-24 15:46:36 -0500

    append new merge_Seq col into df

--- Scripts/House/mergeSequence.R
+++ Scripts/House/mergeSequence.R
@@ -9,6 +9,7 @@ p1 <- rbind(s1_df, s2_df)
 table(p1$seq_par) 
 
 ## return TRUE if 2 sequences in df can be merged into 1
+## HAVE NOT TURN STREET CHECK ON
 mergeSeq <- function(df){
   
   if(df$SEQ %>% unique() %>% length != 2){
@@ -26,8 +27,11 @@ mergeSeq <- function(df){
 # return true if the proportion of parity after merge does not exceed 10%
 chkPar <- function(p1){
   prop <- table(p1$seq_par)[1]/sum(table(p1$seq_par))
-  if (length(table(p1$seq_par)) == 1) return(TRUE)
-  else if (min(prop, 1- prop) < 0.1) return(TRUE)
+  # if (length(table(p1$seq_par)) == 1) return(TRUE)
+  # else if (min(prop, 1- prop) < 0.1) return(TRUE)
+  # else return(FALSE)
+  if(length(table(p1$seq_par)) == 1) return(TRUE)
+  else if (min(table(p1$seq_par)[1], table(p1$seq_par)[2]) > 2) return(TRUE)
   else return(FALSE)
 }
 
@@ -47,6 +51,26 @@ chkJump <- function(p1, jump_size){
   }
   return(TRUE)
 }
+
+seq_list <- HN6 %>% pull(SEQ) %>% unique() %>% na.omit
+
+## return yes if seq i can be joined with seq i+1
+merge_list <- sapply(seq(1,length(seq_list)-1), function(i){
+  s_df <- sample_df %>% filter(SEQ %in% c(seq_list[i], seq_list[i+1]))
+  mergeSeq(s_df)
+})
+
+HN7 <- HN6 %>% mutate(merge_SEQ = SEQ)
+for(i in seq(2, length(seq_list))){
+  if(merge_list[i-1]){
+    HN7 <- HN7 %>% mutate(merge_SEQ = ifelse(SEQ == seq_list[i], NA, merge_SEQ))
+  }
+}
+HN7 <- HN7 %>% fill(merge_SEQ, .direction = "down")
+HN7 %>% filter(merge_SEQ == 12) %>% View
+
+HN7 %>% pull(SEQ) %>% unique() %>% na.omit() %>% length
+HN7 %>% pull(merge_SEQ) %>% unique() %>% na.omit() %>% length
 
 chkPar(p1)
 chkRoad(p1)
