b20e6466eb54ebcbd37b90c1379b3e73a00e5091
gl2668 <gl2668@columbia.edu>
2019-12-10 16:19:06 -0500

    Create 00_Street_Spell_Check_MNBKR.R

---
+++ Scripts/Street/00_Street_Spell_Check_MNBKR.R
@@ -0,0 +1,20 @@
+###### Spell Check ######
+
+# Required Libraries
+library(dplyr)
+library(hunspell)
+
+# Load Data
+# rawData <- read_csv("...")
+
+# Check Spelliing Function
+checkSpelling <- function(rawData) {
+  distinctStreetNames <- unique(rawData$`Street address 2`)
+  misspeltStreets <- distinctStreetNames %>%
+    hunspell() %>%
+    unlist() %>%
+    unique() %>%
+    sort()
+  print(misspeltStreets)
+  print(paste0(length(misspeltStreets), " Possible Misspellings"))
+}
