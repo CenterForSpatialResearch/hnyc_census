1611ff74ce04fce93415b0fb10dbd5963a4bca8c
bojum <54489225+bojum@users.noreply.github.com>
2019-12-11 21:17:31 -0500

    upload 03_Matched_Street_Fill_Down

---
+++ Scripts/Street/03_Matched_Street_Fill_Down.R
@@ -0,0 +1,21 @@
+
+#' fillDownStreet
+#' 
+#' Fill down cleaned street names within the same enumeration page and ED.
+#' This function is to be run on output from `02_Street_Matching_MNBK.R`.
+#' @param df A dataframe with `microfilm` and `ED` columns.
+#' @return A dataframe with cleaned street filled down within an enumeration 
+#' page and ED (group_by(`microfilm`, `ED`)).
+fillDownStreet <- function(df){
+  
+  #' This line can be removed if df has only H record.
+  df <- df %>% filter(record == "H")
+  
+  x <- df %>% group_by(microfilm, ED) %>%
+    fill(best_match, .direction="down") %>%
+    ungroup()
+  return(x)
+}
+
+#' sample_cleaned is an output from `02_Street_Matching_MNBK.R`
+sample_st_filled <- fillDownStreet(sample_cleaned)
