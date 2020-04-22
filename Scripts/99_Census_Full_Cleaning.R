ca26d6d860a45c4a28991c50cebc23e97dcd0728
clintonwxy <cwxy.clinton@gmail.com>
2020-04-22 19:40:19 +0800

    Removed Sys.time() for 99_Census_Full_Cleaning.R
    File ready for use on HPC

--- Scripts/99_Census_Full_Cleaning.R
+++ Scripts/99_Census_Full_Cleaning.R
@@ -1,7 +1,7 @@
 # Importing libraries into libPath, as Columbia HPC does not allow installation of
 # packages into their storage space. COMMENT OUT if running on local computer.
 .libPaths("/rigel/home/cxw2002/rpackages") # Ammend "cxw2002" to user account 
-.libPaths()
+.libPaths() # To check if appending was successful
 
 # Import all packages
 library(hunspell)
@@ -20,41 +20,14 @@ edict <- read_csv("full_mn_dict.csv")
 sample <- read_csv("census_sample.csv")
 hn_dict <- clean_hn_dict("combine_mn.csv")
 
-# Load all functions
-## To run all scripts. Sys.time() is used on the HPC to evaluate function run time.
-## A later iteration would likely remove Sys.time once code has been evaluated.
 
-# 1. Street Match
-currentTime = Sys.time()
-output <- street_match(sample, edict)
-Sys.time() - currentTime
-currentTime = Sys.time()
+# Running all functions
+output <- street_match(sample, edict) %>%
+  fillDownStreet(.) %>%
+  house_clean(., edict, hn_dict) %>%
+  fillDownHouseNum(.) %>%
+  street_type_builder(.) %>%
+  build_Address(.)
 
-# 2. Filling Down Street Number
-output <- fillDownStreet(output)
-Sys.time() - currentTime
-currentTime = Sys.time()
-
-# 3. Cleaning House Number
-output <- house_clean(output, edict, hn_dict)
-Sys.time() - currentTime
-currentTime = Sys.time()
-
-# 4. Filling Down House Number
-output <- fillDownHouseNum(output)
-Sys.time() - currentTime
-currentTime = Sys.time()
-
-# 5. Appending Street Type
-output <- street_type_builder(output)
-Sys.time() - currentTime
-currentTime = Sys.time()
-
-# 6. Constructing Completed Address
-output <- build_Address(output)
-Sys.time() - currentTime
-currentTime = Sys.time()
-
-# 7. Writing Output to CSV, COMMENT OUT if not needed
+# Writing Output to CSV, COMMENT OUT if not needed
 write_csv(output, "output.csv")
-Sys.time() - currentTime
