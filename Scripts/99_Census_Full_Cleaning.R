28c51d8b89bd8b1270c37f84234226fe176e621f
Dan Miller <miller.daniel.patrick@gmail.com>
2020-06-04 17:16:28 -0400

    updated full run for BK on HPC

--- Scripts/99_Census_Full_Cleaning.R
+++ Scripts/99_Census_Full_Cleaning.R
@@ -1,6 +1,6 @@
 # Importing libraries into libPath, as Columbia HPC does not allow installation of
 # packages into their storage space. COMMENT OUT if running on local computer.
-.libPaths("../rpackages")
+.libPaths("../../../census/rpackages")
 .libPaths() # To check if appending was successful
 
 # Import all packages
@@ -15,25 +15,28 @@ source("04_house_clean.R")
 source("05_House_Number_Fill_Down.R")
 source("06_Address_builder.R")
 
+# Set up to run for BK 1910 Census EDIT FOR EACH NEW RUN - edited 20200604 BK
 # Import data
-edict <- read_csv("https://raw.githubusercontent.com/CenterForSpatialResearch/hnyc_census/master/Street_Dict/full_mn_dict.csv")
-hn_dict <- read_csv("https://raw.githubusercontent.com/CenterForSpatialResearch/hnyc_census/master/Street_Dict/combine_mn.csv") %>%
+# Street Dictionary EDIT TO TOGGLE MN OR BK DICTS
+edict <- read_csv("https://raw.githubusercontent.com/CenterForSpatialResearch/hnyc_census/master/Street_Dict/full_bk_dict.csv")
+hn_dict <- read_csv("https://raw.githubusercontent.com/CenterForSpatialResearch/hnyc_census/master/Street_Dict/combine_bk.csv") %>%
   select(Left_Low = y1910Left_Low,
          Left_High = y1910Left_High,
          Right_Low = y1910Right_Low,
          Right_High = y1910Right_High,
          everything()) %>%
   clean_hn_dict()
-sample <- read_csv("../data/input/census_sample.csv")
+#Census Data EDIT INPUT CSV TO TOGGLE MN OR BK INPUT
+census_data <- read_csv("../Data/input/census_1910_h_bk.csv")
 
 
 # Running all functions
-output <- street_match(sample, edict) %>%
+output <- street_match(census_data, edict) %>%
   fillDownStreet(.) %>%
   house_clean(., edict, hn_dict) %>%
   fillDownHouseNum(.) %>%
   street_type_builder(.) %>%
   build_Address(.)
 
-# Writing Output to CSV, COMMENT OUT if not needed
-write_csv(output, "../data/output/output_0505.csv")
+# Writing Output to CSV, COMMENT OUT if not needed EDIT to reflect MN BK and VERSION
+write_csv(output, "../Data/output/output_bk_0604.csv")
